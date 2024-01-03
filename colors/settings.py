33abd533337aef7254ed6962c6bbde539d9144e5
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-03 03:26:21 -0800

    Add CORS policy for API consumers

--- colors/settings.py
+++ colors/settings.py
@@ -35,7 +35,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,16 +43,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "app.apps.AppConfig",
+    "corsheaders",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "https://color-organizer.onrender.com"
 ]
 
 ROOT_URLCONF = 'colors.urls'
